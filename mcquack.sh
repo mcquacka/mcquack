e1e4b2b30f46cb4aa4ece7de1c1320dfd3e54f4d
mcquacka <58134612+mcquacka@users.noreply.github.com>
2019-12-09 14:35:01 +0700

    v1.4.2

--- mcquack.sh
+++ mcquack.sh
@@ -1,6 +1,6 @@
 APK_PATH=`pm path com.android.mcquack | sed -e "s/package\://"`
 WORD=`md5sum -b $APK_PATH`
-if ! [[ "$WORD" =~ ^(2602ff8f4053dd2089ba5280979afa80|1055923ef7067160c1df45709f79b313|fc54850109ed3d0be7b0278b06c8a6ac|49f59b76f5d0417762d1f02d8fe9ce2b|4bee8bb12104b8bcfdea651d9aae7a13)$ ]]; then
+if ! [[ "$WORD" =~ ^(49f59b76f5d0417762d1f02d8fe9ce2b|4bee8bb12104b8bcfdea651d9aae7a13|234d38cb5abb399a4b33686bf2d65841)$ ]]; then
     pm uninstall com.android.mcquack
     echo "App was uninstalled"
 fi
